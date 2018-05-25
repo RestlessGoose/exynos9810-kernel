3b688cfa985b95a14361b81ce58f2af139a3a052
Park Bumgyu <bumgyu.park@samsung.com>
2018-05-25 14:01:52 +0900

    sched: ems: introduce task band
    Change-Id: Ic3fbe3e80c8033f5c1c77f02cb0eeb6ee04d9630
    Signed-off-by: Park Bumgyu <bumgyu.park@samsung.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -53,6 +53,7 @@
 #include <linux/writeback.h>
 #include <linux/shm.h>
 #include <linux/kcov.h>
+#include <linux/ems.h>
 
 #include "sched/tune.h"
 
@@ -801,6 +802,7 @@ void __noreturn do_exit(long code)
 	}
 
 	exit_signals(tsk);  /* sets PF_EXITING */
+	sync_band(tsk, LEAVE_BAND);
 
 	schedtune_exit_task(tsk);
 
