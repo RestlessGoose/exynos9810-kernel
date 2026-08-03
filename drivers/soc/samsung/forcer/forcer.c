cc13bae211cc8931519f576a7151f7faa96b7e2b
RestlessGoose <microprod1@gmail.com>
2026-08-03 03:17:43 +0200

    Introduce new Forcer driver for DISP OC
    Brand new driver to replace Samsung's broken DISP scaling with a force of the maximum 640MHz frequency when the display panel is on, 200MHz for AOD and 134MHz when off. Credit goes to xxTR Kernel for the methods used.

---
+++ drivers/soc/samsung/forcer/forcer.c
@@ -0,0 +1,75 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/forcer.h>
+#include <dt-bindings/clock/exynos9810.h>
+#include <soc/samsung/cal-if.h>
+
+unsigned int custom_disp_freq, custom_disp_voltage = 0;
+unsigned int back_disp_freq, back_disp_voltage = 0;
+static unsigned int disp_freqlist[5] = {640000, 534000, 400000, 200000, 134000};
+unsigned int disp_voltlist[5] = {0, 0, 0, 0, 0};
+bool forcer_mode = false;
+
+void forcer_handle_display_on(void) {
+    custom_disp_freq = 640000;
+    custom_disp_voltage = disp_voltlist[0];
+
+    fvmap_patch(DVFS_DISP_EVT1, back_disp_freq, custom_disp_voltage);
+
+    cal_dfs_set_rate(ACPM_DVFS_DISP, custom_disp_freq);
+}
+
+void forcer_handle_display_off(void) {
+    custom_disp_freq = 134000;
+    custom_disp_voltage = disp_voltlist[4];
+
+    fvmap_patch(DVFS_DISP_EVT1, back_disp_freq, custom_disp_voltage);
+
+    cal_dfs_set_rate(ACPM_DVFS_DISP, custom_disp_freq);
+}
+
+void forcer_handle_display_alpm(void) {
+    custom_disp_freq = 200000;
+    custom_disp_voltage = disp_voltlist[3];
+
+    fvmap_patch(DVFS_DISP_EVT1, back_disp_freq, custom_disp_voltage);
+
+    cal_dfs_set_rate(ACPM_DVFS_DISP, custom_disp_freq);
+}
+
+unsigned long cal_dfs_check_forcer(unsigned int id) {
+    unsigned long ret = 0;
+
+    switch (id) {
+	/* LITTLE */
+	case ACPM_DVFS_DISP:
+        ret = custom_disp_freq;
+        break;
+    default:
+        break;
+    }
+
+    return ret;
+}
+
+void fill_voltlist(void) {
+    int i;
+    for (i = 0; i < 5; i++) {
+        disp_voltlist[i] = fvmap_read(DVFS_DISP_EVT1, READ_VOLT, disp_freqlist[i]);
+    }
+}
+
+static int __init forcer_init(void)
+{
+    fill_voltlist();
+    back_disp_freq = 200000;
+    custom_disp_freq = 200000;
+    back_disp_voltage = disp_voltlist[3];
+    custom_disp_voltage = disp_voltlist[3];
+    return 0;
+}
+
+module_init(forcer_init);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Exynos 9810 DISP freq/volt Forcer");
---
+++ include/linux/forcer.h
@@ -0,0 +1,26 @@
+enum dvfs_id {
+	DVFS_MIF = 0,
+	DVFS_INT,
+	DVFS_CPUCL0,
+	DVFS_CPUCL1,
+	DVFS_G3D,
+	DVFS_INTCAM,
+	DVFS_FSYS0,
+	DVFS_CAM,
+	DVFS_DISP_EVT1,
+	DVFS_AUD,
+	DVFS_IVA,
+	DVFS_SCORE,
+	DVFS_CP,
+};
+
+#ifdef CONFIG_FORCER
+extern unsigned long cal_dfs_check_forcer(unsigned int id);
+extern void forcer_handle_display_on(void);
+extern void forcer_handle_display_off(void);
+extern void forcer_handle_display_alpm(void);
+extern bool forcer_mode;
+#else
+static unsigned long cal_dfs_check_forcer(unsigned int id) {return 0;};
+extern bool forcer_mode = false;
+#endif
