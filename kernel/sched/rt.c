ea8f04c3aab0cfd7de4004f8e2b8074d0be213c1
RestlessGoose <microprod1@gmail.com>
2026-08-08 03:22:57 +0200

    (uclamp) Fixups

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -3718,6 +3718,7 @@ const struct sched_class rt_sched_class = {
 
 #ifdef CONFIG_RT_GROUP_SCHED
 	.task_change_group	= task_change_group_rt,
+#endif
 
 #ifdef CONFIG_UCLAMP_TASK
 	.uclamp_enabled		= 1,
